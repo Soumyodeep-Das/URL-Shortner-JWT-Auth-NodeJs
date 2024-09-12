e8ffc82666d1e8c2bff964aa5bcfb6b1f8effc9a
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-12 18:53:25 +0530

    JWT Authorization Incorporated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,49 @@
 const { getUser } = require("../service/auth")
 
-async function restrictToLoggedInUserOnly(req, res, next) {
-    const userUid = req.cookies.uid
-    if (!userUid) return res.redirect("/login")
-    const user = getUser(userUid)
-    if (!user) return res.redirect("/login")
-    req.user = user
-    next()
+function checkForAuthentication( req, res, next ){
+    const authorizationHeaderValue = req.headers["authorization"]
+    req.user = null;
+    if (
+        !authorizationHeaderValue ||
+        !authorizationHeaderValue.startsWith("Bearer")
+    )
+    return next()
+
+    const token = authorizationHeaderValue.split("Bearer ")[1]
+    const user = getUser(token)
+
+    req.user = user;
+    return next()
+
 }
 
-async function checkAuth(req, res, next) {
-    const userUid = req.cookies.uid
-    const user = getUser(userUid)
-    req.user = user
-    next()
+function restrictTo(roles){
+    return function (req, res, next){
+        if(!req.user) return res.redirect("/login");
+        if(!roles.includes(req.user.role)) return res.end("UnAuthorized")
+        
+        return next()
+    }
 }
+// async function restrictToLoggedInUserOnly(req, res, next) {
+//     const userUid = req.cookies.uid
+//     if (!userUid) return res.redirect("/login")
+//     const user = getUser(userUid)
+//     if (!user) return res.redirect("/login")
+//     req.user = user
+//     next()
+// }
+
+// async function checkAuth(req, res, next) {
+//     const userUid = req.cookies.uid
+//     const user = getUser(userUid)
+//     req.user = user
+//     next()
+// }
 
 module.exports = {
-    restrictToLoggedInUserOnly,
-    checkAuth
+    // restrictToLoggedInUserOnly,
+    // checkAuth
+    checkForAuthentication,
+    restrictTo,
 }
